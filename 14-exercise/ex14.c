66f559e828760e71ea1ee9b3609cbba23b86d71b
Josiah Berkebile <praenato14@gmail.com>
2017-07-05 23:20:56 -0500

    ex14: extra-cred don't use \0 terminator

--- 14-exercise/ex14.c
+++ 14-exercise/ex14.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // Forward declarations so that you can call these
 // functions prior to their definitions.
 int can_print_it(char ch);
-void print_letters(char argv[]);
+void print_letters(size_t length, char argv[]);
 
 void print_arguments(int argc, char* argv[]) {
   for (int i = 0; i < argc; i++) {
-    print_letters(argv[i]);
+    print_letters(strlen(argv[i]), argv[i]);
   }
 }
 
-void print_letters(char arg[]) {
-  for (int i = 0; arg[i] != '\0'; i++) {
+void print_letters(size_t length, char arg[]) {
+  for (unsigned int i = 0; i < length; i++) {
     char ch = arg[i];
 
     if (isalpha(ch) || isblank(ch)) {
