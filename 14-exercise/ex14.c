0c79187076f765e158db61604943ef451b66ac8d
Josiah Berkebile <praenato14@gmail.com>
2017-07-05 23:14:31 -0500

    ex14: extra-credit get rid of can_print_it

--- 14-exercise/ex14.c
+++ 14-exercise/ex14.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
 
+// Forward declarations so that you can call these
+// functions prior to their definitions.
 int can_print_it(char ch);
 void print_letters(char argv[]);
 
@@ -14,16 +16,12 @@ void print_letters(char arg[]) {
   for (int i = 0; arg[i] != '\0'; i++) {
     char ch = arg[i];
 
-    if (can_print_it(ch)) {
+    if (isalpha(ch) || isblank(ch)) {
       printf("'%c' == %d ", ch, ch);
     }
   }
 
   printf("\n");
-}
-
-int can_print_it(char ch) {
-  return isalpha(ch) || isblank(ch);
 }
 
 int main(int argc, char* argv[]) {
